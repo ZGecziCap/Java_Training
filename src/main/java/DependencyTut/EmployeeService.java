453dd280053a2288d4d0d4e2a668340ba83b9082
zgeczi <zoltan.geczi@capgemini.com>
2020-04-15 10:31:16 +0200

    Properties

--- src/main/java/DependencyTut/EmployeeService.java
+++ src/main/java/DependencyTut/EmployeeService.java
@@ -1,6 +1,8 @@
 package DependencyTut;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,16 +12,28 @@ public class EmployeeService {
 
     private final ApplicationEventPublisher publisher;
 
-    public EmployeeService(EmployeeDao employeeDao, ApplicationEventPublisher publisher) {
+    //egyik megoldás
+    private final Environment env;
+    //másik, ebben az esetben még a proerties file-ban látszik is ha van használatban az adott érték, vagyis van @Value
+    private final boolean uppercaseEnabled;
+
+    public EmployeeService(EmployeeDao employeeDao, ApplicationEventPublisher publisher,Environment env, @Value("${uppercase.enabled}") boolean uppercaseEnabled) {
         this.employeeDao = employeeDao;
         this.publisher = publisher;
+        this.env = env;
+        this.uppercaseEnabled = uppercaseEnabled;
     }
 
     public void saveEmployee(String name) {
         if (name.isBlank()) {
             throw new IllegalArgumentException("Name can not be empty");
         }
-        var upperCase = name.toUpperCase();
+
+        var upperCase = name;
+        if (Boolean.parseBoolean(env.getProperty("uppercase.enabled"))) {
+            upperCase = name.toUpperCase();
+        }
+
         employeeDao.saveEmployee(upperCase);
 
         publisher.publishEvent(new EmployeeHasCreatedEvent(this, name));
